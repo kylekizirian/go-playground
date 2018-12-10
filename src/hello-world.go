8d6503104bdb0fb65da13007ea62f12f085d5100
kylekizirian <kyle.kizirian@gmail.com>
2018-12-09 19:45:27 -0500

    wrote function to find largest number under passed parameter

---
+++ src/hello-world.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func findLargestPrime(max uint64) uint64 {
+
+	//TODO assert input is valid
+
+	start := time.Now()
+
+	primeList := make([]uint64, 1)
+	primeList[0] = 2
+
+	for i := uint64(3); i <= max; i = i + 2 {
+		// check if prime
+		isPrime := true
+		for _, num := range primeList {
+			if i%num == 0 {
+				isPrime = false
+				break
+			}
+		}
+		if isPrime {
+			primeList = append(primeList, i)
+		}
+	}
+	endTime := time.Since(start)
+	fmt.Printf("It took %s to find the largest prime under %d\n", endTime, max)
+	return primeList[len(primeList)-1]
+}
+
+func main() {
+	var primeUnder uint64 = 1000000
+	largestPrimeUnder := findLargestPrime(primeUnder)
+	fmt.Printf("Largest prime under %d = %d\n", primeUnder, largestPrimeUnder)
+}
