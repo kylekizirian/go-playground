5dcec62d246fc906eba3fba95641b678602add5b
kylekizirian <kyle.kizirian@gmail.com>
2019-02-28 18:57:16 -0500

    implemented hello world

--- src/hello-world.go
+++ src/hello-world.go
@@ -1,43 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"math"
-	"time"
-)
-
-func findLargestPrime(max uint64) uint64 {
-
-	//TODO assert input is valid
-
-	start := time.Now()
-
-	primeList := make([]uint64, 1)
-	primeList[0] = 2
-
-	for i := uint64(3); i <= max; i = i + 2 {
-		// check if prime
-		isPrime := true
-		for _, num := range primeList {
-			if i%num == 0 {
-				isPrime = false
-				break
-			}
-			if float64(num) > math.Sqrt(float64(i)) {
-				break
-			}
-		}
-		if isPrime {
-			primeList = append(primeList, i)
-		}
-	}
-	endTime := time.Since(start)
-	fmt.Printf("It took %s to find the largest prime under %d\n", endTime, max)
-	return primeList[len(primeList)-1]
-}
+import "fmt"
 
 func main() {
-	var primeUnder uint64 = 100000000
-	largestPrimeUnder := findLargestPrime(primeUnder)
-	fmt.Printf("Largest prime under %d = %d\n", primeUnder, largestPrimeUnder)
+	fmt.Println("hello world!")
 }
